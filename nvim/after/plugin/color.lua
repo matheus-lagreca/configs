abb3076a6a0a0911fdc6a981b97eec598bcc02d7
matheus-lagreca <matheus.lagreca@gmail.com>
2022-12-02 01:13:14 -0300

    Delete nvim directory

--- nvim/after/plugin/color.lua
+++
@@ -1,8 +0,0 @@
-print("Tokyo night config")
-
-vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_tranparent = true
-
-vim.g.backgroung = "dark"
-
-vim.cmd("colorscheme tokyonight")
--- nvim/lua/mattster/remap.lua
+++
@@ -1,3 +0,0 @@
-local nnoremap = require("mattster.keymap").nnoremap
-
-nnoremap("<leader>pv", "<cmd>Ex<CR>")
